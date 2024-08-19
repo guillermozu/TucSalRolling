651bf4121b95073a4785f886ac3c05a0441d6a3d
guillermo zurita <guillermozu@gmail.com>
2024-08-19 00:11:52 -0300

    se agregaron pagina varias y se las vinculó

---
+++ js/global.js
@@ -0,0 +1,10 @@
+let usuario = JSON.parse(localStorage.getItem("user")) || null;
+
+//Si está la opción rol o no
+if (usuario?.rol !== "admin") {
+  document.getElementById("administrador").innerHTML = "";
+}
+
+function irMascota(id) {
+  location.href = `../pages/mascota.html?id=${id}`;
+}
